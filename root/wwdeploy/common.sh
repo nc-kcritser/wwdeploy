76325f0242c53095d07a74839cc3bffa4aa484eb
ken.critser <ken.critser@dell.com>
2025-12-04 17:44:59 +0000

    Updating file references

--- root/wwdeploy/common.sh
+++ root/wwdeploy/common.sh
@@ -4,10 +4,10 @@
 
 # Source OpenHPC and Warewulf variables so they are available to all scripts.
 # Note: The paths to these files must be correct relative to the execution directory.
-if [ -f /root/openhpc-set-vars.sh ]; then
-    source /root/openhpc-set-vars.sh
+if [ -f /root/wwdeploy/customer-set-vars.sh ]; then
+    source /root/wwdeploy/customer-set-vars.sh
 else
-    echo "Warning: /root/openhpc-set-vars.sh not found."
+    echo "Warning: /root/wwdeploy/customer-set-vars.sh not found."
 fi
 
 if [ -f /root/wwdeploy/reference_external_downloads.config ]; then
