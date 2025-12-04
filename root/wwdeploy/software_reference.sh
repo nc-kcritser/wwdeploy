2ce75fdaaf6c889134e58d1c1b653bcdd1a6a639
ken.critser <ken.critser@dell.com>
2025-12-04 17:47:20 +0000

    Removing unneeded file

--- root/wwdeploy/software_reference.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-
-EPEL_URL="https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm"
-EPEL_URL="https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm"
